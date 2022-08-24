918696d88bd2565fc3bba79a9ab4c33a5b70f48c
Сергей Трегуб <sergey.tregub@vocord.ru>
2022-08-24 17:49:47 +0300

    Add job controller

---
+++ src/otel-webapi-service/otel-webapi-service/Controllers/JobController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace otel_webapi_service.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class JobController : ControllerBase
+    {
+        static Random random = new Random();
+
+        /// <summary>
+        /// The action synchronously runs a task for 0 - 500 msec before returns an answer
+        /// </summary>
+        [HttpGet("RunLongJob")]
+        public IActionResult RunLongJob()
+        {
+            var delay = random.Next(500);
+
+            Thread.Sleep(delay);
+
+            return Ok($"The job takes {delay} msec");
+        }
+
+        /// <summary>
+        /// The action synchronously runs a task for 600 - 1000 msec before returns an answer
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet("RunVeryLongJob")]
+        public IActionResult RunVeryLongJob()
+        {
+            var delay = random.Next(600, 1000);
+
+            Thread.Sleep(delay);
+
+            return Ok($"The job takes {delay} msec");
+        }
+    }
+}
